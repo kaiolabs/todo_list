9845ebcfe90df428fbb2485287afe09d9522bb4d
kaiolabs <peypro1@gmail.com>
2022-11-15 21:19:00 -0300

    tipos de variáveis do sqlite para monta o db

---
+++ lib/src/core/infra/enums/db_types_variables.dart
@@ -0,0 +1,11 @@
+class DBTypesVariables {
+  static const idType = "INTEGER PRIMARY KEY AUTOINCREMENT";
+  static const varcharType = "VARCHAR(80) DEFAULT ''";
+  static const booleanType = "BOOLEAN DEFAULT 0";
+  static const intType = "INT DEFAULT 0";
+  static const doubleType = "DOUBLE DEFAULT 0.0";
+  static const charType = "CHAR(5) DEFAULT ''";
+  static const dateType = "DATE";
+  static const dateTimeType = "DATETIME";
+  static const textType = "TEXT";
+}
