7b1ea3a205b9a09e736829fc11f28906c8de24ae
kaiolabs <peypro1@gmail.com>
2022-11-15 21:53:39 -0300

    Definindo o ColorOutlet

---
+++ lib/src/core/presenters/theme/color_outlet.dart
@@ -0,0 +1,31 @@
+import 'package:flutter/material.dart';
+
+abstract class ColorOutlet {
+  static const colorPrimary = Color(0xFF7E37BB);
+  static const colorPrimaryLight = Color(0xFFAD54FB);
+  static const Color colorWhite = Color(0xFFFFFFFF);
+  static const Color colorShadow = Color(0x824B4949);
+  static const Color colorGrey = Color(0xFF686877);
+  static const Color colorGreyLight = Color(0xFFE5E5E5);
+  static const Color colorTransparent = Color(0x00000000);
+  static const Color colorBlack = Color(0xFF000000);
+  static const Color colorBlackLight = Color(0xFF373737);
+  static const colorLightPurple = Color(0xFF5D577E);
+  static const colorDarkPurple = Color(0xFF27214D);
+
+  //card colors
+  static const Color colorCardBlack = Color(0xFF1E1E1E);
+  static const Color colorCardBlue = Color(0xFF437DCE);
+  static const Color colorCardBrown = Color(0xFFA37F5F);
+  static const Color colorCardGray = Color(0xFF86869E);
+  static const Color colorCardGreen = Color.fromARGB(255, 84, 178, 102);
+  static const Color colorCardBlueOceane = Color.fromARGB(255, 110, 202, 202);
+  static const Color colorCardGreenDark = Color(0xFF2E7D32);
+  static const Color colorCardPink = Color(0xFFD95F9F);
+  static const Color colorCardPurple = Color(0xFFAD54FB);
+  static const Color colorCardRed = Color(0xFFD95F5F);
+  static const Color colorCardWhite = Color(0xFFCDCDCD);
+  static const Color colorCardYellow = Color(0xFFE8C54F);
+
+  static const Color colorGlass = Color(0x7F000000);
+}
