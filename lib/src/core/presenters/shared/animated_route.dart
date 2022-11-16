625ce1b97626444888f34477111e6676da4be79c
kaiolabs <peypro1@gmail.com>
2022-11-15 21:28:25 -0300

    Criando o animatedRoute

---
+++ lib/src/core/presenters/shared/animated_route.dart
@@ -0,0 +1,21 @@
+import 'package:flutter/material.dart';
+
+Route animatedRoute(Widget page) {
+  return PageRouteBuilder(
+    pageBuilder: (context, animation, secondaryAnimation) => page,
+    transitionsBuilder: (context, animation, secondaryAnimation, child) {
+      var begin = const Offset(1.0, 0.0);
+      var end = Offset.zero;
+      var curve = Curves.ease;
+      var tween = Tween(begin: begin, end: end).chain(CurveTween(curve: curve));
+
+      return FadeTransition(
+        opacity: animation,
+        child: SlideTransition(
+          position: animation.drive(tween),
+          child: child,
+        ),
+      );
+    },
+  );
+}
