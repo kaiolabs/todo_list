4a00a80cf3946b8ffa728c3c558e9f631c0f2b21
kaiolabs <peypro1@gmail.com>
2022-11-20 16:35:54 -0300

    Refatorando o main

--- lib/main.dart
+++ lib/main.dart
@@ -4,11 +4,16 @@ import 'package:todo_list/app_module.dart';
 import 'package:todo_list/app_widget.dart';
 import 'package:todo_list/src/core/repositories/db.dart';
 import 'package:todo_list/src/core/services/create_tables.dart';
+import 'package:todo_list/src/modules/base/controllers/preferences_theme.dart';
 
 void main() async {
   WidgetsFlutterBinding.ensureInitialized();
   await DB.initizateDb();
   await CreateTables.createTables();
+  await DB.initSettings();
+  await DB.getDarkMode().then((value) {
+    PreferencesTheme.brightness = value ? ValueNotifier(Brightness.dark) : ValueNotifier(Brightness.light);
+  });
   Modular.to.addListener(() {
     debugPrint('Modular.to: ${Modular.to}');
   });
