f4ed2edcdba368a982666fd010cb6a2b579d73e3
kaiolabs <peypro1@gmail.com>
2022-11-20 16:31:07 -0300

    Criando os termos de uso do aplicativo

---
+++ lib/src/core/presenters/terms/terms_of_use.dart
@@ -0,0 +1,29 @@
+class TermsOfUse {
+  static const String _termsOfUse = '''
+## Termos de Uso
+
+A sua privacidade é importante para nós. É política do Minhas tarefas respeitar a sua privacidade em relação a qualquer informação que possamos coletar no app Minhas tarefas, e outros apps que possuímos e operamos.\n\n
+Solicitamos informações pessoais apenas quando realmente precisamos delas para lhe fornecer um serviço. Fazemo-lo por meios justos e legais, com o seu conhecimento e consentimento. Também informamos por que estamos coletando e como será usado.\n\n
+Apenas retemos as informações coletadas pelo tempo necessário para fornecer o serviço solicitado. Quando armazenamos dados, os protegemos dentro de meios comercialmente aceitáveis ​​para evitar perdas e roubos, bem como acesso, divulgação, cópia, uso ou modificação não autorizados.\n\n
+Não compartilhamos informações de identificação pessoal publicamente ou com terceiros, exceto quando exigido por lei.\n\n
+O nosso app pode ter links para sites externos que não são operados por nós. Esteja ciente de que não temos controle sobre o conteúdo e práticas desses sites e não podemos aceitar responsabilidade por suas respectivas políticas de privacidade.\n\n
+Você é livre para recusar a nossa solicitação de informações pessoais, entendendo que talvez não possamos fornecer alguns dos serviços desejados.\n\n
+O uso continuado de nosso site será considerado como aceitação de nossas práticas em torno de privacidade e informações pessoais. Se você tiver alguma dúvida sobre como lidamos com dados do usuário e informações pessoais, entre em contacto conosco.\n\n
+
+## Compromisso do Usuário
+
+O usuário se compromete a fazer uso adequado dos conteúdos e da informação que o Minhas tarefas oferece no app e com caráter enunciativo, mas não limitativo:
+
+- A) Não se envolver em atividades que sejam ilegais ou contrárias à boa fé a à ordem pública;
+- B) Não difundir propaganda ou conteúdo de natureza racista, xenofóbica, casas de apostas ou azar, qualquer tipo de pornografia ilegal, de apologia ao terrorismo ou contra os direitos humanos;
+- C) Não causar danos aos sistemas físicos (hardwares) e lógicos (softwares) do Minhas tarefas, de seus fornecedores ou terceiros, para introduzir ou disseminar vírus informáticos ou quaisquer outros sistemas de hardware ou software que sejam capazes de causar danos anteriormente mencionados.
+
+## Mais informações
+
+Esperemos que esteja esclarecido e, como mencionado anteriormente, se houver algo que você não tem certeza se precisa ou não, geralmente é mais seguro deixar os cookies ativados, caso interaja com um dos recursos que você usa em nosso site.
+  
+Esta política é efetiva a partir de 15 de novembro de 2020.
+''';
+
+  static String get termsOfUse => _termsOfUse;
+}
